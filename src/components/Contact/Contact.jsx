8972eb5bf9a9bf952c4d7908c931dd71d7eadefa
ghernandez24 <halex24a@bu.edu>
2021-05-13 23:28:59 -0400

    fixed email bug

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
               target="_blank"
               rel="noopener noreferrer"
               className="cta-btn cta-btn--resume"
-              href={email}
+              href={email ? `mailto:${email}` : ''}
             >
               {btn || 'Email me'}
             </a>
